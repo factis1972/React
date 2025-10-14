2ebff3565cef05a46be99958d67e117e52a2cd52
Factis1972 <foa1972@hotmail.com>
2025-10-14 12:04:41 -0300

    ok

--- src/components/EjerciciosClase3/EquipoTalentoLab.jsx
+++ src/components/EjerciciosClase3/EquipoTalentoLab.jsx
@@ -3,7 +3,7 @@ import styles from './EquipoTalentoLab.module.css';
 const EquipoTalentoLab = ({index, equipo}) => {
     const {nombre, rol, imagen} = equipo;
     return(
-        <div className={styles.empleados}>
+        <div className={styles.empleado}>
             <img key={index} src={imagen} style={{ width: "150px", height: "150px" }}></img> 
             <h3>Nombre: {nombre}, Rol: {rol}</h3>
         </div>
